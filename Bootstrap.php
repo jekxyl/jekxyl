6ff082c7c69dc5ed864caf64ea8ee5419b412627
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2016-05-10 16:16:43 +0200

    Bootstrap: Look for another autoloader location.

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 
-require 'vendor/autoload.php';
+if (true === file_exists(__DIR__ . '/vendor/autoload.php')) {
+    require __DIR__ . '/vendor/autoload.php';
+} elseif (true === file_exists(__DIR__ . '/../../autoload.php')) {
+    require __DIR__ . '/../../autoload.php';
+} else {
+    die('Cannot find Composer autoloader.');
+}
 
 use Hoa\Protocol;
 
